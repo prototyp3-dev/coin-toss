6169f7ad084d8c09bbc0ef21bedb7f40ca2d78aa
Kirill Igumenshchev <kirill.igum@gmail.com>
2023-11-22 13:45:13 -0500

    Fixed a typo in the variable name for the input prompt.

--- trust-and-teach.py
+++ trust-and-teach.py
@@ -83,7 +83,7 @@ def handle_advance(data):
         # conversation_id, prompt = decode_abi(['uint256','string'], binary)
         # player1, player2 = decode_abi(["address", "address"], gamekey)
 
-        promptLLMResponse = submitPrompt(inputInput)
+        promptLLMResponse = submitPrompt(promptInput)
         # result = toss_coin(seed)
 
         # winner = None
