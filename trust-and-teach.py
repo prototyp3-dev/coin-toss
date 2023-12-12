15ff262f12c27cf549e56c78f7d780f7de444eed
Kirill Igumenshchev <kirill.igum@gmail.com>
2023-12-11 21:49:44 -0800

    Removed commented out code and fixed variable names in trust-and-teach.py.

--- trust-and-teach.py
+++ trust-and-teach.py
@@ -133,22 +133,15 @@ def handle_advance(data):
                             "promptInput": promptInput,
                             "promptLLMResponseNumber": i,
                             "promptLLMResponseSplit": j,
-                            # "promptLLMResponse": promptLLMResponse
                             "promptLLMResponse": promptLLMResponses[i][j]
                         }
                 logger.info(f">>>>>>>nnnnnspsp> <> notice: {notice}")
                 post("notice", {"payload": str2hex(json.dumps(notice))})
                 notices += [ notice ]
 
-                # voucher_payload = announcePromptResponse + encode_abi(["uint256", "string[]"], [conversationId, promptLLMResponses])
-                voucher_payload = announcePromptResponse + encode_abi(["uint256", "string[]"], [conversationId, promptLLMResponse])
-                # # voucher_payload = announcePromptResponse + encode_abi(["uint256", "string[]"], [conversationId, promptLLMResponses[i][j]])
+                voucher_payload = announcePromptResponse + encode_abi(["uint256", "string[]"], [conversationId, promptLLMResponses[i][j]])
                 voucher = {"destination": promptAuthor_addr, "payload": "0x" + voucher_payload.hex()}
                 post("voucher", voucher)
-
-        # voucher_payload = announcePromptResponse + encode_abi(["uint256", "string[]"], [conversationId, promptLLMResponse])
-        # voucher = {"destination": promptAuthor_addr, "payload": "0x" + voucher_payload.hex()}
-        # post("voucher", voucher)
 
     except Exception as e:
         status = "reject"
