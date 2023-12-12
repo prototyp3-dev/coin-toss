836db7cabd570b85e2ee0fce7ee1aab47beb02de
Kirill Igumenshchev <kirill.igum@gmail.com>
2023-12-11 19:41:29 -0800

    Refactored code to simplify and optimize promptLLMResponse handling.

--- trust-and-teach.py
+++ trust-and-teach.py
@@ -91,33 +91,50 @@ def handle_advance(data):
         conversationId, promptInput = decode_abi(['uint256', 'string'], binary)
         logger.info(f"Received promptInput: {promptInput}, from conversationId: {conversationId}")
 
-        promptLLMResponses = []
-        n_responses = 1
-        response_split_length = 512
-        for i in range(n_responses):
-            promptLLMResponse_whole = [submitPrompt(promptInput)]
-            # split the response into a list of strings of 512 characters
-            promptLLMResponse_splits = [promptLLMResponse_whole[0][i:i+response_split_length] for i in range(0, len(promptLLMResponse_whole[0]), response_split_length)]
-            promptLLMResponses += [ promptLLMResponse_splits ]
-        logger.info(f">>>>>>>> <> promptLLMResponses: {promptLLMResponses}")
+        promptLLMResponse1 = submitPrompt(promptInput)
+        promptLLMResponse2 = submitPrompt(promptInput)
+        promptLLMResponse = [promptLLMResponse1,promptLLMResponse2]
 
-        notices = []
-        for i in range(len(promptLLMResponses)):
-            for j in range(len(promptLLMResponses[i])):
-                notice = {
-                            "conversationId": conversationId,
-                            "promptAuthor": promptAuthor_addr,
-                            "promptInput": promptInput,
-                            "promptLLMResponseNumber": i,
-                            "promptLLMResponseSplit": j,
-                            "promptLLMResponse": promptLLMResponses[i][j]
-                        }
-                post("notice", {"payload": str2hex(json.dumps(notice))})
-                notices += [ notice ]
+        notice = {
+            "conversationId": conversationId,
+            "promptAuthor": promptAuthor_addr,
+            "promptInput": promptInput,
+            "promptLLMResponse": promptLLMResponse
+        }
 
-                voucher_payload = announcePromptResponse + encode_abi(["uint256", "string[]"], [conversationId, promptLLMResponses[i][j]])
-                voucher = {"destination": promptAuthor_addr, "payload": "0x" + voucher_payload.hex()}
-                post("voucher", voucher)
+        post("notice", {"payload": str2hex(json.dumps(notice))})
+
+        voucher_payload = announcePromptResponse + encode_abi(["uint256", "string[]"], [conversationId, promptLLMResponse])
+        voucher = {"destination": promptAuthor_addr, "payload": "0x" + voucher_payload.hex()}
+        post("voucher", voucher)
+
+        # promptLLMResponses = []
+        # n_responses = 1
+        # response_split_length = 512
+        # for i in range(n_responses):
+        #     promptLLMResponse_whole = [submitPrompt(promptInput)]
+        #     # split the response into a list of strings of 512 characters
+        #     promptLLMResponse_splits = [promptLLMResponse_whole[0][i:i+response_split_length] for i in range(0, len(promptLLMResponse_whole[0]), response_split_length)]
+        #     promptLLMResponses += [ promptLLMResponse_splits ]
+        # logger.info(f">>>>>>>> <> promptLLMResponses: {promptLLMResponses}")
+
+        # notices = []
+        # for i in range(len(promptLLMResponses)):
+        #     for j in range(len(promptLLMResponses[i])):
+        #         notice = {
+        #                     "conversationId": conversationId,
+        #                     "promptAuthor": promptAuthor_addr,
+        #                     "promptInput": promptInput,
+        #                     "promptLLMResponseNumber": i,
+        #                     "promptLLMResponseSplit": j,
+        #                     "promptLLMResponse": promptLLMResponses[i][j]
+        #                 }
+        #         post("notice", {"payload": str2hex(json.dumps(notice))})
+        #         notices += [ notice ]
+
+        #         voucher_payload = announcePromptResponse + encode_abi(["uint256", "string[]"], [conversationId, promptLLMResponses[i][j]])
+        #         voucher = {"destination": promptAuthor_addr, "payload": "0x" + voucher_payload.hex()}
+        #         post("voucher", voucher)
 
     except Exception as e:
         status = "reject"
