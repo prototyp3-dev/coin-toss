2c26ad73916948dc9017942fc4cb87e3f726f3ec
Kirill Igumenshchev <kirill.igum@gmail.com>
2023-12-14 17:59:14 -0800

    Fix voucher iteration to start from 0 instead of 1.

--- contract_rw_depexec.fish
+++ contract_rw_depexec.fish
@@ -108,7 +108,7 @@ function test_cartesi_voucher
       yarn start notice list &| tee -a $logfile
       yarn start voucher list &| tee -a $logfile
       set voucherTotal ( yarn start voucher list &| tee -a $logfile | sed -n '4p' | jq ".| length")
-      for iVoucher in (seq 1 $voucherTotal)
+      for iVoucher in (seq 0 ( math $voucherTotal -1 ))
         yarn start voucher execute --index $iVoucher --input 0 &| tee -a $logfile
       end
       cd -
