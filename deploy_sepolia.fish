8ffd441f2e73fb6a95acf55f429f1cd85fa70a8e
Ubuntu <ubuntu@ip-172-31-7-92.us-west-2.compute.internal>
2024-01-23 09:21:12 +0000

    Added a new script file `deploy_sepolia.fish` with commands for deploying and running Docker containers.

---
+++ deploy_sepolia.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/env fish
+docker buildx bake -f docker-bake.hcl -f docker-bake.override.hcl machine --load --set \*.args.NETWORK=sepolia
+DAPP_NAME="coin-toss" docker compose --env-file env.sepolia -f deploy-testnet.yml up #deploy to cartesi factory
+DAPP_NAME="coin-toss" docker compose --env-file env.<network> -f deploy-testnet.yml down -v #stop docker after finish
+DAPP_NAME="trust-and-teach" docker compose --env-file env.sepolia -f docker-compose-testnet.yml up
+DAPP_NAME="coin-toss" docker compose --env-file env.<network> -f docker-compose-testnet.yml -f docker-compose-host-testnet.yml up # host mode
+docker run --rm --net="host" ghcr.io/foundry-rs/foundry "cast send --private-key $PLAYER1_PRIVATE_KEY --rpc-url $RPC_URL $COIN_TOSS_ADDRESS \"set_dapp_address(address)\" $DAPP_ADDRESS"
+docker run --rm --net="host" ghcr.io/foundry-rs/foundry "cast send --private-key $PLAYER1_PRIVATE_KEY --rpc-url $RPC_URL $COIN_TOSS_ADDRESS \"sendInstructionPrompt(string)\" \"hi. \""
+
+
