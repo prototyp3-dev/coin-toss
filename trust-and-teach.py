440661548dfb814b00822bc1f67ebb60c5f24488
Kirill Igumenshchev <kirill.igum@gmail.com>
2024-01-29 13:17:31 -0500

    Update PROMPT_CMD_tail in trust-and-teach.py.

--- trust-and-teach.py
+++ trust-and-teach.py
@@ -31,7 +31,8 @@ announcePromptResponse = k.update(b'announcePromptResponse(uint256,uint256,uint2
 
 # logger.info(f"HTTP rollup_server url is {rollup_server}")
 PROMPT_CMD_head = "./run stories15M.bin -t 0.8 -n 4 -i '"
-PROMPT_CMD_tail = "' | sed 's/[^a-zA-Z ]//g' ; exit 0"
+PROMPT_CMD_tail = "' ; exit 0"
+# PROMPT_CMD_tail = "' | sed 's/[^a-zA-Z ]//g' ; exit 0"
 # PROMPT_CMD_tail = "' 2>/dev/null | sed 's/[^a-zA-Z ]//g' | tr -d \\n ; exit 0"
 
 def hex2str(hex):
