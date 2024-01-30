014f43ceabe802fea4d66b09e324654b2d31cd27
Kirill Igumenshchev <kirill.igum@gmail.com>
2024-01-30 12:55:43 -0500

    Add llmSteps parameter to sendInstructionPrompt and cartesiSubmitPrompt functions.

--- contracts/src/localhost/trust-and-teach.sol
+++ contracts/src/localhost/trust-and-teach.sol
@@ -20,6 +20,7 @@ contract TrustAndTeach {
     struct Conversation {
         address author;
         string prompt;
+        uint256 llmSteps;
         string[][] responses;
         uint256 rankSubmissionCount;
         mapping(address => RankSubmission) rankSubmissions;
@@ -40,25 +41,38 @@ contract TrustAndTeach {
         L2_DAPP = l2_dapp;
     }
 
-    function sendInstructionPrompt(string memory prompt) public {
+    function sendInstructionPrompt(uint256 llmSteps, string memory prompt)
+        public
+    {
         // require(L2_DAPP != address(0));
         Conversation storage conversation = conversations[
             current_conversation_id
         ];
         conversation.author = msg.sender;
         conversation.prompt = prompt;
+        conversation.llmSteps = llmSteps;
         conversation.createInstructionTimestamp = block.timestamp;
-        cartesiSubmitPrompt(current_conversation_id, prompt);
-        emit PromptSent(current_conversation_id, prompt);
+        cartesiSubmitPrompt(current_conversation_id, llmSteps, prompt);
+        emit PromptSent(current_conversation_id, llmSteps, prompt);
         current_conversation_id++;
     }
 
-    function cartesiSubmitPrompt(uint256 conversation_id, string memory prompt)
-        public
-    {
+    function cartesiSubmitPrompt(
+        uint256 conversation_id,
+        uint256 llmSteps,
+        string memory prompt
+    ) public {
+        // bytes memory payload = abi.encode(conversation_id, llmSteps, prompt);
         bytes memory payload = abi.encode(conversation_id, prompt);
         inputBox.addInput(L2_DAPP, payload);
     }
+
+    // function cartesiSubmitPrompt(uint256 conversation_id, string memory prompt)
+    //     public
+    // {
+    //     bytes memory payload = abi.encode(conversation_id, prompt);
+    //     inputBox.addInput(L2_DAPP, payload);
+    // }
 
     function getPromptByConversationId(uint256 conversation_id)
         public
@@ -222,7 +236,8 @@ contract TrustAndTeach {
     }
 
     event RankSubmitted(uint256 conversation_id, address user, uint256[] ranks);
-    event PromptSent(uint256 conversation_id, string prompt);
+    event PromptSent(uint256 conversation_id, uint256 llmSteps, string prompt);
+    // event PromptSent(uint256 conversation_id, string prompt);
     event PromptResponseAnnounced(
         uint256 conversation_id,
         uint256 iResponse,
